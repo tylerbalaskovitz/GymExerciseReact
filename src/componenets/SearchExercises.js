9eacd793c43bb31cdeca087eb10d3a42b1b3d6cb
tylerbalaskovitz <tylerbalaskovitz@yahoo.com>
2022-12-13 11:27:01 -0500

    addeded more component data

--- src/componenets/SearchExercises.js
+++ src/componenets/SearchExercises.js
@@ -1,8 +1,18 @@
 import React, {useEffect, useState}from 'react'
 import {Box, Button, Stack, TextField, Typography} from '@mui/material'
+import { fontSize } from '@mui/system'
 
 
 const SearchExercises = () => {
+
+    const [search, setSearch] = useState('')
+    const handleSearch = async() => {
+
+        if (search){
+           // const exercisesData = await fetchData();
+        }
+    }
+
   return (
     <Stack alignItems="center" mt="37px"
     justifyContent="center" p="20px">
@@ -12,13 +22,38 @@ const SearchExercises = () => {
         </Typography>
         <Box position="relative" mb="72px">
         <TextField 
+        sx={{
+            input: 
+            { fontWeight: '700',
+            border: 'none',
+            borderRadius:'4px'
+            },
+            width: { lg: '1000', xs: '350px'},
+            backgroundColor: '#fff',
+            borderRadius: '40px'
+        }}
         height="76px"
-        value=""
-        onChange= {(e) => {}}
+        value={search}
+        onChange= {(e) => {setSearch(e.target.value.toLowerCase)}}
         placeholder="Search Exercises"
         type="text"
         />
+        <Button className="search-btn"
+        sx={{
+            bgcolor:'#FF2625',
+            color:'#fff',
+            textTransform: 'none',
+            width: {lg: '175px', xs: '80px'},
+            fontSize: {lg: '20px', xs: '14px'},
+            height: '56px',
+            position:'absolute',
+            right: '0'
 
+        }}
+        onClick={handleSearch}
+        >
+        Search
+        </Button>
         </Box>
     </Stack>
   )
